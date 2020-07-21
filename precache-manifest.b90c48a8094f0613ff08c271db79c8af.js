903539fd2ac060b45415ad407b377a768d367ee9
ethmcc <64378116+infinj@users.noreply.github.com>
2020-07-20 21:13:53 -0400

    Publish 7/20/20 9:13pm

---
+++ precache-manifest.b90c48a8094f0613ff08c271db79c8af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e500e247492fe3312ddfd4e35aee67e6",
+    "url": "/turing/index.html"
+  },
+  {
+    "revision": "853b5c855458e8432b8f",
+    "url": "/turing/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7f6904c6e77e84b0bae0",
+    "url": "/turing/static/css/main.12b18630.chunk.css"
+  },
+  {
+    "revision": "853b5c855458e8432b8f",
+    "url": "/turing/static/js/2.1bfe339a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/turing/static/js/2.1bfe339a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f6904c6e77e84b0bae0",
+    "url": "/turing/static/js/main.e0415866.chunk.js"
+  },
+  {
+    "revision": "bc25397052f43381a7cd",
+    "url": "/turing/static/js/runtime-main.55d68dd3.js"
+  }
+]);
